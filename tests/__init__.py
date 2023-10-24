d563ff3e607c41c2da62e102b353b43c380e5805
Joel Pendleton <joelkieronpendleton@gmail.com>
2023-10-24 16:43:35 +0100

    Added in task toolkit

---
+++ tests/__init__.py
@@ -0,0 +1,9 @@
+import filecmp
+
+import two_qubit_transmon_auto_tuning
+
+print(
+    "Sonali is a tests/__init__.pytests/__init__.pytests/__init__.pytests/\
+        __init__.pytests/__init__.pytests/__init__.pytests/__init__.pytests/\
+        __init__.py"
+)
