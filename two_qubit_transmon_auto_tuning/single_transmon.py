c9a81ba595be5068ff4b8589906347939cf8074f
100-nali <sonali.goel@st-hughs.ox.ac.uk>
2023-10-26 15:06:16 +0100

    Edited the single-transmon simulator; fixed H.

--- two_qubit_transmon_auto_tuning/single_transmon.py
+++ two_qubit_transmon_auto_tuning/single_transmon.py
@@ -1,3 +1,4 @@
+#Import relevant modules
 import numpy as np
 import matplotlib.pyplot as plt
 from qutip import *
@@ -6,30 +7,33 @@ from qutip import *
 omega_q = 1.0
 anharm = 0.2
 
-#Define number of energy levels
-dim = 3
+#Define number of energy levels (dim = 3 if accounting for leakage to 3rd level)
+dim = 2
 
-# Create the qubit basis states; 3 accounts for the 3rd energy level that can be leaked into
-g = basis(3, 0)  # Ground state |0⟩
-e = basis(3, 1)  # Excited state |1⟩
+# Create the qubit basis states; 
+g = basis(dim, 0)  # Ground state |0⟩
+e = basis(dim, 1)  # Excited state |1⟩
 
 # Define operators
-c1 = destroy(dim)
-c2 = c1.dag
-c2c1 = tensor(c2,c1)
+c1: Qobj = destroy(dim) 
+c2: Qobj = c1.dag()
+c2c1 = c1*c2
 
 # Create the qubit Hamiltonian
 # H_qubit = (omega_qubit * (sigmaz() / 2)) + (anharmonicity * sigmaz() * sigmaz() / 2)
-H_drift = (omega_q*c2c1) + 0.5*anharm*c2c1*(c2c1-1)
+H_drift = (omega_q*c2c1) + 0.5*anharm*c2c1*(c2c1-qeye(dim))
 
 # Initial state (e.g., qubit in the ground state)
 psi0 = g
+rho0 = ket2dm(psi0)
+
+print(psi0)
 
 # Time points for simulation
 tlist = np.linspace(0, 10, 100)
 
 # Time evolution of the qubit
-result = mesolve(H_drift, psi0, tlist, [], [sigmax(), sigmay(), sigmaz()])
+result = mesolve(H_drift, rho0, tlist, [], [sigmax(), sigmay(), sigmaz()])
 
 # Plot the results
 plt.figure()
