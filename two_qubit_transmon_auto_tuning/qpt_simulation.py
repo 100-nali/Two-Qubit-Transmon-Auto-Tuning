4f79380613733d25f3a62fb1a4d4ccdeb64f288d
100-nali <sonali.goel@st-hughs.ox.ac.uk>
2023-11-02 13:25:54 +0000

    Updated process_fidelity evaluation. Updated pulse defining function. Don't know yet if H description is accurate.

--- two_qubit_transmon_auto_tuning/qpt_simulation.py
+++ two_qubit_transmon_auto_tuning/qpt_simulation.py
@@ -7,10 +7,10 @@ from dataclasses import dataclass
 import math
 
 #Define number of energy levels
-dim = 2
+dim:int         = 2
 
 #Define qubit 1 parameters
-w_q1: float     = 1.0
+w_q1: float     = 1.0 
 a_q1: float     = 1.0           #anharmonicity for qubit 1
 r1:float        = 1.0           #drive coupling for qubit 1
 
@@ -23,34 +23,42 @@ r2:float        = 1.0           #drive coupling for qubit 2
 g: float        = 1.0
 
 #Define pulse parameters - q1
-w_d1            = 1.0           #drive frequency for qubit 1
-amp_1           = 1.0           #pulse amplitude for qubit 1
-mean_1          = 0             #mean of gaussian pulse for qubit 1
-std_1           = 1.0           #std of gaussian pulse for qubit 1
+w_d1:float      = 1.0                   #drive frequency for qubit 1
+amp_1:float     = 0           #pulse amplitude for qubit 1
+mean_1:float    = 0                     #mean of gaussian pulse for qubit 1
+std_1:float     = 1.0                   #std of gaussian pulse for qubit 1
 
 
 #Define pulse parameters - q2
-w_d2            = 1.0
-amp_2           = 1.0
-mean_2          = 0
-std_2           = 1.0
+w_d2:float      = 1.0    
+amp_2:float     = 0
+mean_2:float    = 0
+std_2:float     = 1.0
 
 
 #phi
-phi1            = np.pi/4       #Phase angle for I1 and Q1
-phi2            = np.pi/4       #Phase angle for I2 and Q2
+phi1            = np.pi/2              #Phase angle for I1 and Q1
+phi2            = np.pi/2               #Phase angle for I2 and Q2
 
 
 #Define time-stamps
-T               = g / (2*np.pi)                 #Final time step to propagate over
-nT              = 100                           #Number of time steps to propagate over 
+T               = 500                        #Final time step to propagate over
+nT              = 500                           #Number of time steps to propagate over (doesnt affect t_real)
 t               = np.linspace(0, T, nT)         #time vector
 
-#Define driving functions
-v_1 = lambda t, *args: amp_1 * np.exp(-(t - mean_1) ** 2 / (2 * std_1 ** 2)) / (
-                std_1 * np.sqrt(2 * np.pi)) * np.exp(1j*(w_d1*t + phi1))
-v_2 = lambda t, *args: amp_2 * np.exp(-(t - mean_2) ** 2 / (2 * std_2 ** 2)) / (
-                std_2 * np.sqrt(2 * np.pi)) * np.exp(1j*(w_d2*t + phi2))
+#Define driving pulses
+def wave_shape(w_d, phi, t):
+    x = np.exp(1j*(w_d*t + phi))
+    return x
+
+def pulse_shape(amp, mean, std, t):
+    x = 1.0* amp / (np.sqrt(2.0 * np.pi) * std) * np.exp(-np.power((t - mean) / std, 2.0) / 2)
+    return x
+
+# v_1 = wave_shape(w_d1, phi1, t) * pulse_shape(amp_1, mean_1, std_1, t)
+# v_2 = wave_shape(w_d2, phi2, t) * pulse_shape(amp_2, mean_2, std_2, t)
+v_1 = wave_shape(w_d1,phi1,t) * pulse_shape(amp_1, mean_1, std_1, t)
+v_2 = wave_shape(w_d2,phi2,t) * pulse_shape(amp_2, mean_2, std_2, t)
 
 #Define a1, a2
 a1 = tensor(destroy(dim), qeye(dim))
@@ -87,7 +95,7 @@ H_d2 = [r2*sigma_y2 ,v_2]
 
 H = [H_0, H_d1, H_d2]
 
-#%% iSWAP
+#%% iSWAP ###########################
 
 #  Define the iSWAP gate 
 U_psi_iSWAP = II * (0.5+0j) + XX * 0.5j + YY * 0.5j + ZZ * (0.5+0j)             #for state vector
@@ -95,11 +103,11 @@ U_rho_iSWAP = spre(U_psi_iSWAP) * spost(U_psi_iSWAP.dag())                      
 
 #Find ideal process matrix
 chi_ideal_iSWAP = qpt(U_rho_iSWAP, op_basis)
-fig = qpt_plot_combined(chi_ideal_iSWAP, op_label, r'$i$SWAP')
-plt.show()
+# fig = qpt_plot_combined(chi_ideal_iSWAP, op_label, r'$i$SWAP')
+# plt.show()
 
 
-#%% Cphase
+#%% Cphase ###########################
 
 #Define U: TODO: convert to tensor product form
 U_psi_cphase = Qobj([[1, 0, 0, 0],
@@ -107,24 +115,57 @@ U_psi_cphase = Qobj([[1, 0, 0, 0],
                 [0, 0, 1, 0],
                 [0, 0, 0, -1]])
 
+
 U_rho_cphase = spre(U_psi_cphase) * spost(U_psi_cphase.dag())
 chi_ideal_cphase = qpt(U_rho_cphase, op_basis)
 
-fig = qpt_plot_combined(chi_ideal_cphase, op_label, r'$CPHASE')
-plt.show()
+# fig = qpt_plot_combined(chi_ideal_cphase, op_label, r'$CPHASE')
+# plt.show()
 
-# %% QPT over unknown quantum process
+
+
+#%% CNOT  ###########################
+U_psi_CNOT = Qobj([[1, 0, 0, 0],
+       [0, 1, 0, 0],
+        [0, 0, 0, 1],
+        [0, 0, 1, 0]])
+
+U_rho_CNOT = spre(U_psi_CNOT) * spost(U_psi_CNOT.dag())
+chi_ideal_CNOT = qpt(U_rho_CNOT, op_basis)
+# fig = qpt_plot_combined(chi_ideal_CNOT, op_label, r'$CPHASE')
+# plt.show()
+
+
+# %% QPT over unknown quantum process  ###########################
 U_psi_real = qutip.propagator(H,t)                      #List of lists due to time dependence.
-U_psi_real = U_psi_real[nT-1]                           #Take entry from last time step
-U_rho_real = spre(U_psi_real) * spost(U_psi_real.dag())
+U_psi_real_T = U_psi_real[nT-1]                           #Take entry from last time step
+U_rho_real = spre(U_psi_real_T) * spost(U_psi_real_T.dag())
 chi_real = qpt(U_rho_real, op_basis)
 
 fig = qpt_plot_combined(chi_real, op_label, r'$Actual Process$')
 plt.show()
 
-#%% Evaluate process fidelity
-U1 = U_rho_real
-U2 = U_rho_iSWAP
+#%% Evaluate process fidelity  ###########################
 
-gate_fidelity = process_fidelity(U_rho_real, U_rho_iSWAP, normalize=True)
-print('Process Fidelity at T = ', gate_fidelity)
+def process_fidelity_s(chi_ideal, chi_real):
+    # Calculate the square root of the ideal process matrix
+    sqrt_chi_ideal = chi_ideal.sqrtm()
+
+    # Calculate the matrix square root of sqrt_chi_ideal * chi_real * sqrt_chi_ideal
+    sqrt_term = sqrt_chi_ideal * chi_real * sqrt_chi_ideal
+
+    # Calculate the trace of the square root term
+    trace_sqrt_term = sqrt_term.tr()
+
+    # Calculate the process fidelity using the formula
+    fidelity = (np.abs(trace_sqrt_term) ** 2).real
+
+    return fidelity
+
+# Calculate the process fidelity
+
+chi_ideal = Qobj(chi_ideal_iSWAP)
+chi_real = Qobj(chi_real)
+fidelity = process_fidelity_s(chi_ideal, chi_real)
+
+print('Process Fidelity at T = ', fidelity)
